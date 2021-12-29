22eda651ecd6a32452a0eb4f52e16c640147382d
Elisa <elisacanyelles@gmail.com>
2021-12-29 10:29:06 +0100

    created nav components, worked on main header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/navigation/MainNavigation';
+
 
 const App = () => {
   return (
-  <Switch>
-<Route exact path='/'><Users/></Route>
-<Route path='/places/new'><NewPlace/></Route>
-<Redirect to='/'/>
-</Switch>
-)}
+    <>
+      <MainNavigation />
+      <main>
+      <Switch>
+
+        <Route exact path='/'><Users /></Route>
+        <Route path='/places/new'><NewPlace /></Route>
+        <Redirect to='/' />
+      </Switch>
+      </main>
+    </>
+  )
+}
+
 
 export default App;
